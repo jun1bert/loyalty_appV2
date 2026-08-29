24e6828d832c93a6bd0c7d25932294640cf27937
juni <junibertanuay0@gmail.com>
2026-08-29 15:29:24 +0800

    V3.01

--- config/services.php
+++ config/services.php
@@ -35,10 +35,4 @@ return [
         ],
     ],
 
-    'services_sync' => [
-        'url' => env('SERVICES_SYNC_URL'),
-        'token' => env('SERVICES_SYNC_TOKEN'),
-        'timeout' => env('SERVICES_SYNC_TIMEOUT', 20),
-    ],
-
 ];
